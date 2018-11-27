583bdbdfb97e7e081a26178d01040da7be347746
wangsb <2885129077@qq.com>
2018-11-27 18:59:13 +0800

    自定义属性解析

--- sys-web/src/main/java/com/springboot/tpl/util/HtmlRegUtils.java
+++ sys-web/src/main/java/com/springboot/tpl/util/HtmlRegUtils.java
@@ -1,11 +1,14 @@
 package com.springboot.tpl.util;
 
 import com.alibaba.fastjson.JSONObject;
+import com.springboot.common.busi.BaseRunTimeException;
 import com.springboot.common.busi.SpringContextHolder;
+import com.springboot.common.util.DocumentUtil;
 import com.springboot.tpl.service.AbstractTplService;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -19,6 +22,11 @@ public class HtmlRegUtils {
 
     // 两种模式，为了应对换行的匹配
     private static final Pattern LIST_PATTERN = Pattern.compile(LIST_REGEX, Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+
+    // 属性解析
+    private static final String ATTR_REGEX = "\\[mine:([\\w]+)([^\\]]*)\\]";
+    // 只区分大小写
+    public static final Pattern ATTR_PATTERN = Pattern.compile(ATTR_REGEX, Pattern.CASE_INSENSITIVE);
 
     /**
      * 标签体 ADD REASON. <br/>
@@ -104,10 +112,39 @@ public class HtmlRegUtils {
         return result;
     }
 
+    /**
+     * 解析属性，并对属性进行预处理操作
+     */
+    public static String parseProperty(String content, Object obj) {
+        // 结果
+        StringBuffer buffer = new StringBuffer();
+        // 正则匹配
+        Matcher m = ATTR_PATTERN.matcher(content);
+        // 查找
+        while (m.find()) {
+            String key = m.group(1).trim();// 字段
+            String param = m.group(2).trim();// 参数
+            // 参数map
+            Map<String, String> paramMap = DocumentUtil.parseText(param);
+            System.out.println("属性：" + key + "参数：" + paramMap);
+            // 预处理字段值
+            for (Map.Entry<String, String> entry : paramMap.entrySet()) {
+                try {
+
+                } catch (Throwable e) {
+                    throw new BaseRunTimeException("预处理[" + entry.getKey() + "]后台处理逻辑不存在."+ e.getMessage());
+                }
+            }
+            // 由于$出现在replacement中时，表示对捕获组的反向引用，所以要对上面替换内容中的$进行替换
+            m.appendReplacement(buffer, Matcher.quoteReplacement(obj.toString()));
+        }
+        m.appendTail(buffer);
+        return buffer.toString();
+    }
+
     public static void main(String[] args) {
-        String content = "测试模板<div>{mine:aa num=1 name=test}{/mine:aa}{mine:bb num=1 name=test}{/mine:bb}{mine:cc a=1 b=2/}</div>";
+        String content = "测试标签<div>[mine:title length=\"30\"]\n{mine:aa num=1 name=test}{/mine:aa}{mine:bb num=1 name=test}{/mine:bb}{mine:cc a=1 b=2/}</div>";
         System.out.println(parseLabel(content));
-        String regEx = "(?s)^(?=.*\\d.*\\d.*\\d).+$";
-        System.out.println(Pattern.matches(regEx,"dqwdq123w"));
+        System.out.println(parseProperty(content,"211221" ));
     }
 }
